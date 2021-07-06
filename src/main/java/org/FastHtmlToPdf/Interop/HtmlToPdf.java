922d124994ef1805f3ba2c60b0da3d2ad25902e5
weizhonzhen <czdswzz@126.com>
2021-07-06 18:09:18 +0800

    Add files via upload

--- src/main/java/org/FastHtmlToPdf/Interop/HtmlToPdf.java
+++ src/main/java/org/FastHtmlToPdf/Interop/HtmlToPdf.java
@@ -1,10 +1,20 @@
 package Org.FastHtmlToPdf.Interop;
 
+import com.sun.jna.Callback;
 import com.sun.jna.Library;
 import com.sun.jna.Pointer;
 import com.sun.jna.ptr.PointerByReference;
 
 public interface HtmlToPdf extends Library {
+
+    interface StringCallback extends Callback {
+        void callback(Pointer converter, String str);
+    }
+
+    interface IntCallback extends Callback {
+        void callback(Pointer converter, int i);
+    }
+
     void wkhtmltopdf_init(int use_graphics);
 
     Pointer wkhtmltopdf_create_global_settings();
@@ -22,7 +32,24 @@ public interface HtmlToPdf extends Library {
 
     int wkhtmltopdf_convert(Pointer converter);
 
+    void wkhtmltopdf_set_warning_callback(Pointer converter, StringCallback callback);
+
+    void wkhtmltopdf_set_error_callback(Pointer converter, StringCallback callback);
+
+    void wkhtmltopdf_set_progress_changed_callback(Pointer converter, IntCallback callback);
+
+    void wkhtmltopdf_set_finished_callback(Pointer converter, IntCallback callback);
+
     long wkhtmltopdf_get_output(Pointer converter, PointerByReference data);
+
+
+    int wkhtmltopdf_current_phase(Pointer converter);
+
+    int wkhtmltopdf_phase_count(Pointer converter);
+
+    String wkhtmltopdf_phase_description(Pointer converter, int phase);
+
+    String wkhtmltopdf_progress_string(Pointer converter);
 
 
     void wkhtmltopdf_destroy_converter(Pointer converter);
